d1edaf01997dd226079911e98dea01cac86b340e
potusvn <tuantai.cse@gmail.com>
2019-02-27 02:17:55 +0100

    Spliting dataset after one hot encoding

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ DATA_RAW_ROOT = os.path.join(PROJECT_ROOT, 'data', 'raw')
 DATA_PREPROCESSED_ROOT = os.path.join(PROJECT_ROOT, 'data', 'pre_processed')
 DATA_ENGINEER_ROOT = os.path.join(PROJECT_ROOT, 'data', 'engineered')
 DATA_EXTERNAL_ROOT = os.path.join(PROJECT_ROOT, 'data', 'external')
+DATA_SPLIT_ROOT = os.path.join(PROJECT_ROOT, 'data', 'data_spliting')
 
 MODELS_ROOT = os.path.join(PROJECT_ROOT, 'models')
 
--- src/models/DefaultAlgorithm.py
+++ src/models/DefaultAlgorithm.py
@@ -4,6 +4,7 @@ import pandas as pd
 from os.path import join
 
 from catboost import CatBoostClassifier
+from pandas import DataFrame
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
@@ -18,55 +19,18 @@ from settings import *
 
 
 def main():
-    dataset = pd.read_csv(join(DATA_ENGINEER_ROOT, 'ks-projects-201801_engineered.csv'))
 
-    # ['name', 'category', 'main_category', 'currency', 'deadline', 'goal',
-    #  'launched', 'backers', 'country', 'usd pledged', 'usd_pledged_real',
-    #  'usd_goal_real', 'success', 'contain_special_symbols', 'name_length',
-    #  'num_vowels', 'num_cap', 'num_whitespace', 'contain_bad_word',
-    #  'polarity', 'subjectivity', 'launch_month', 'duration',
-    #  'pledged_per_backer']
+    train_set = join(DATA_SPLIT_ROOT, 'train.csv')
+    test_set = join(DATA_SPLIT_ROOT, 'test.csv')
 
-    # Remove duplicate field
-    dataset = dataset.drop(['name', 'usd pledged', 'backers', 'goal', 'usd_pledged_real', 'deadline', 'launched'], axis=1)
+    train = pd.read_csv(train_set, encoding='latin1', low_memory=True)
+    test = pd.read_csv(test_set, encoding='latin1', low_memory=True)
 
-    # Group feature
-    pre_launched_features = ['category', 'main_category', 'currency', 'usd_goal_real', 'country', 'contain_special_symbols', 'name_length',
-     'num_vowels', 'num_cap', 'num_whitespace', 'contain_bad_word',
-     'polarity', 'subjectivity', 'launch_month', 'duration']
-    post_launched_features = ['backers', 'pledged_per_backer']
+    train_features = train.drop(['success'], axis=1)
+    train_targets = train['success']
 
-    # Label encoding
-    gle = LabelEncoder()
-    category_labels = gle.fit_transform(dataset['main_category'])
-    dataset['main_category'] = category_labels
-
-    sub_category_labels = gle.fit_transform(dataset['category'])
-    dataset['category'] = sub_category_labels
-
-    country = gle.fit_transform(dataset['country'])
-    dataset['country'] = country
-
-    currency = gle.fit_transform(dataset['currency'])
-    dataset['currency'] = currency
-
-    # One hot encoding
-    # pd.get_dummies(dataset, prefix=['currency', 'country', 'main_category', 'category'],
-    #                columns=['currency', 'country', 'main_category', 'category'],
-    #                dummy_na=True, drop_first=True)
-
-
-    X = dataset.drop(['success'], axis=1)
-    print('Predictors ', X.columns)
-    y = dataset['success']
-
-    # One hot
-
-    # Splitting the dataset
-    train_features, test_features, train_targets, test_targets = \
-        train_test_split(X, y, test_size=0.3, random_state=0)
-    print('Train size', train_features.shape)
-    print('Test size', test_features.shape)
+    test_features = test.drop(['success'], axis=1)
+    test_targets = test['success']
 
     parser = argparse.ArgumentParser()
     # For whole folder processing
@@ -79,13 +43,15 @@ def main():
         tree = carl.fit(train_features, train_targets)
         print("The CART accuracy is: ", tree.score(test_features, test_targets) * 100, "%")
     elif args.alg == 'xgboost':
-        xgb = XGBClassifier().fit(train_features, train_targets)
-        print("The XGBoost accuracy is: ", xgb.score(test_features, test_targets) * 100, "%")
+        xgb = XGBClassifier()
+        forest = xgb.fit(train_features, train_targets)
+        print("The XGBoost accuracy is: ", forest.score(test_features, test_targets) * 100, "%")
         plot_importance(xgb)
         plt.show()
     elif args.alg == 'rf':
-        rf = RandomForestClassifier().fit(train_features, train_targets)
-        print("The Random Forest accuracy is: ", rf.score(test_features, test_targets) * 100, "%")
+        rf = RandomForestClassifier()
+        forest = rf.fit(train_features, train_targets)
+        print("The Random Forest accuracy is: ", forest.score(test_features, test_targets) * 100, "%")
     elif args.alg == 'catboost':
         cb = CatBoostClassifier().fit(train_features, train_targets)
         print("The Cat Boost accuracy is: ", cb.score(test_features, test_targets) * 100, "%")
